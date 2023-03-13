80d20a61a0959f08ebe674fdd23d4fff7a8e8413
Heisenburgir <127745451+Heisenburgir@users.noreply.github.com>
2023-03-13 16:50:26 +0100

    Add files via upload

---
+++ walterwhite.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int     numero = 308;
+    int     numero2 = 87104;
+    double  numerolungo = 871040000;
+    float   numeroconlavirgola = 2.5;
+    char    carattere = 'w';
+
+    printf("I live at %d Negra Aroyo Lane Albaquerque New Mexico %d\n", numero, numero2);
+    printf("Science b**** %f, I'm Saul Goodman, did you know you have rights? Constitution says you do, and so do I %f\n", numerolungo, numeroconlavirgola);
+    
+    
+}
