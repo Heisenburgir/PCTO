da48608bab4f68054878aa419dcc7371bc2d1544
Heisenburgir <127745451+Heisenburgir@users.noreply.github.com>
2023-03-13 16:49:37 +0100

    Add files via upload

---
+++ jessepinkman.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int y;
+
+    printf("ïnserisci un waltuh: ");
+    scanf("%d", &x);
+    printf("inserisci un altro waltuh: ");
+    scanf("%d", &y);
+    printf("i waltuh inseriti sono x = %d e y = %d\n", x+14, y+104);
+}
