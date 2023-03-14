60ce28cce14a441bd01dbf54fab915a968f9bc65
Heisenburgir <127745451+Heisenburgir@users.noreply.github.com>
2023-03-14 11:18:53 +0100

    Add files via upload

---
+++ Heisenberg.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int età;
+
+    printf("Quanti anni hai: ");
+    scanf("%d", &età);
+    if (età >= 18)
+    {
+        printf("ok allora non molestare la gente ai bar, pervertito\n");
+    }
+    else 
+    {
+        printf("guarda Breaking Bad ed inizia a pensare al tuo futuro da spacciatpore di metanfetamina col tuo professore di chimica che ha il cancro e vuole fare i soldi\n");
+    }
+}    
